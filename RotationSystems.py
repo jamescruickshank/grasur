18ff74861286cba94a2eaf536b0e261e152d2357
jamescruickshank <james.cruickshank@nuigalway.ie>
2018-10-19 00:03:51 +0100

    initial commit

---
+++ RotationSystems.py
@@ -0,0 +1,182 @@
+
+from sage.all import *
+import time
+
+class OrientedRotationSystem(object):
+    """
+sigma is a cycle decomposition of 
+a permutation of darts and tau is a cycle decomposition of 
+a fixed point free involution of darts. Note that the set of 
+darts must be [1,2,...,2*e], so tau and sigma must be permutations of that set 
+for example 
+    r = OrientedRotationSystem([(1,2,3),(4,5,6),(7,8,9),(10,11,12)],[(1,4),(2,12),(3,8),(5,11),(6,7),(9,10)])
+creates OrientedRotationSystem instance corresponding to a copy of K_4 embedded in the torus with a quadrilateral face and an octagonal face
+"""
+    def __init__(self,sigma=[],tau=[]):
+        # first validate the input
+        #u = []
+        #for x in tau:
+        #    u = union(u,list(x))
+        #u.sort()
+        #if u != list(range(1,2*len(tau)+1)):
+        #    raise ValueError(str(u))
+
+        self.perm_group = PermutationGroup([sigma,tau])
+        self.sigma_group,self.tau_group = PermutationGroup([sigma]),PermutationGroup([tau])
+        self.sigma_perm = self.sigma_group.gens()[0]
+        self.tau_perm = self.tau_group.gens()[0]
+        self.number_of_darts = len(self.tau_perm.cycles())*2
+        self.cache = {}
+
+    @classmethod
+    def from_graph(cls,graph,no_isomorphs=True):
+        """returns a list a all possible OrientedRotationSystem instances with underlying graph equal to graph. graph should be an instance of sage.all.Graph"""
+        edges = graph.edges()
+        tau = [(2*i+1,2*i+2) for i in range(len(edges))]
+        v_dict = { v:[] for v in graph.vertices() }
+        for i in range(len(edges)):
+            ed = edges[i]
+            v_dict[ed[0]].append(2*i+1)
+            v_dict[ed[1]].append(2*i+2)
+        c_list = v_dict.values()
+        rs_list = []
+        for sigma in list_of_cycles(c_list):
+            r = cls(sigma,tau)
+            # now check for isomorphs
+            for e in rs_list:
+                if r.is_isomorphic(e):
+                    break
+            else:
+                rs_list.append(r)
+        return rs_list
+
+
+    def components(self):
+        return self.perm_group.orbits()
+
+    def vertices(self):
+        return self.sigma_group.orbits()
+
+
+    def edges(self):
+        return self.tau_group.orbits()
+
+    def faces(self):
+        if self.cache.has_key('faces')==False:
+            g = PermutationGroup([self.sigma_perm*self.tau_perm])
+            self.cache['faces']=g.orbits()
+        return self.cache['faces']
+
+    def undirected_graph(self):
+        """returns the underlying undirected graph object"""
+        vertex_labels = [min(x) for x in self.sigma_group.orbits()]
+        gr = Graph(multiedges=True,loops=True)
+        gr.add_vertices(vertex_labels)
+        for e in self.edges():
+            s = self.sigma_group.orbit(e[0])
+            t = self.sigma_group.orbit(e[1])
+            gr.add_edge([min(s),min(t)])
+
+        return gr
+
+    def face_degrees(self):
+        face_list = self.faces()
+        return[len(i) for i in face_list]
+
+    def f_vector(self,length=4):
+        fds = self.face_degrees()
+        return tuple([fds.count(i+1) for i in range(length)])
+
+
+    def genus(self):
+        return (2 - len(self.faces())+len(self.edges())-len(self.vertices()))/2
+
+    def is_isomorphic(self,other,mapping=False,orientation_preserving=False):
+
+        # first check the underlying graph and then use the face structure???
+        
+        if self.number_of_darts != other.number_of_darts:
+            return False
+        for i in range(1,self.number_of_darts+1):
+            val = self.based_isomorphism(other,1,i,mapping=mapping)
+            if val !=False:
+                return val
+
+        if orientation_preserving==False:
+            rev = OrientedRotationSystem(other.sigma_perm.inverse(),other.tau_perm)
+            return self.is_isomorphic(rev,mapping=mapping,orientation_preserving=True)
+        
+        return False
+
+    def based_isomorphism(self,other,self_dart,other_dart,mapping=False):
+        dart_mapping = {self_dart:other_dart}
+        mapped_darts = [self_dart]
+
+        # extend the mapping over an orbit of a dart, 
+        # check for conflicts with existing mapping (return false)
+        # and extend the mapping to the darts at the other end of the edge
+        # while checking for conflicts
+        # remove all the darts of the orbit from the mapped_darts list
+        # but don't remove the other ends of the edges!
+        while len(mapped_darts)>0:
+            s = mapped_darts[0]
+            t = dart_mapping[s]
+            s_cycle = cycle_of(self.sigma_perm,s)
+            t_cycle = cycle_of(other.sigma_perm,t)
+            if len(s_cycle)!=len(t_cycle):
+                return False
+            for i in range(len(s_cycle)):
+                if dart_mapping.has_key(s_cycle[i]):
+                    if dart_mapping[s_cycle[i]] != t_cycle[i]:
+                        return False
+                dart_mapping[s_cycle[i]]=t_cycle[i]
+                try:
+                    mapped_darts.remove(s_cycle[i])
+                except:
+                    pass
+                if dart_mapping.has_key(self.tau_perm(s_cycle[i])):
+                    if dart_mapping[self.tau_perm(s_cycle[i])]!=other.tau_perm(t_cycle[i]):
+                        return False
+                else:
+                    dart_mapping[self.tau_perm(s_cycle[i])]=other.tau_perm(t_cycle[i])
+                    mapped_darts.append(self.tau_perm(s_cycle[i]))
+
+        if mapping:
+            return dart_mapping
+        else:
+            return True
+        
+
+def cycle_of(perm,n):
+    cycles = perm.cycle_tuples()
+    for a in cycles:
+        if n in a:
+            return a[a.index(n):]+a[:a.index(n)]
+    return (n,)
+
+
+def list_of_cycles(l,pattern=None):
+
+    if len(l) == 1:
+        e = l[0]
+        return [[tuple(e[:1])+tuple(x)] for x in Permutations(e[1:]).list()]
+    else:
+        return [  i+j for j in list_of_cycles(l[1:]) for i in list_of_cycles(l[:1])] 
+
+
+
+
+def maps_from_graph(graph,max_f_vector=(0,0,0),max_genus=None,min_genus=None):
+    l = OrientedRotationSystem.from_graph(graph)
+    o = []
+    length = len(max_f_vector)
+    for r in l:
+        fd = r.f_vector(length)
+        if min([max_f_vector[i]-fd[i] for i in range(length)]) >=0:
+            o.append(r)
+    if max_genus is not None:
+        o = filter(lambda x: x.genus()<=max_genus,o)
+    if min_genus is not None:
+        o = filter(lambda x: x.genus()>=min_genus,o)
+    return o
+
---
+++ Sparsity.py
@@ -0,0 +1,186 @@
+
+from sage.all import *
+import time
+
+
+class NonUniqueLabelError(Exception):
+    pass
+
+
+class MyDiGraph(DiGraph):
+    """Subclass of DiGraph with methods for two cycle splitting and Hennberg 
+    extensions. edges **must** have unique postive integer labels"""
+
+    def assign_edge_labels(self):
+        for i in range(len(self.edges())):
+            pass
+
+    def get_edges_with_label(self,label):
+        """return a list of edges with the given label"""
+        return [e for e in self.edges() if e[2]==label]
+        
+    def get_edge_with_label(self,label):
+        l = self.get_edges_with_label(label)
+        if len(l)>1:
+            raise NonUniqueLabelError()
+        return l[0]
+
+
+
+    def two_cycle_split(self, vertex, darts):
+        """this needs all the edge labels to be unique. darts is a list of pairs (edge_label,j) where j is one of 0 or 1"""
+        v = max(self.vertices())+1
+        w = v+1
+        l = max(self.edge_labels())+1
+        other = MyDiGraph(self,multiedges=True,loops=True)
+        other.add_vertices([v,w])
+        for e in self.incoming_edges(vertex):
+            label = e[2]
+            if e[0]==e[1]:
+                if (label,0) in  darts and (label,1) in darts:
+                    other.add_edge(v,v,l)
+                if (label,0) in  darts and (label,1) not in darts:
+                    other.add_edge(v,w,l)
+                if (label,0) not in  darts and (label,1) in darts:
+                    other.add_edge(w,v,l)
+                if (label,0) not in  darts and (label,1) not in darts:
+                    other.add_edge(w,w,l)
+            else:
+                if (label,1) in darts:
+                    other.add_edge(e[0],v,l)
+                else:
+                    other.add_edge(e[0],w,l)
+            l+=1
+
+        for e in self.outgoing_edges(vertex):
+            label = e[2]
+            if e[0]==e[1]:
+                pass
+            else:
+                if (label,0) in darts:
+                    other.add_edge(v,e[1],l)
+                else:
+                    other.add_edge(w,e[1],l)
+            l+=1
+
+        other.add_edges([(v,w,l),(w,v,l+1)])
+        other.delete_vertex(vertex)
+        return other
+
+
+    def darts_at(self,vertex):
+        return [(e[2],0) for e in self.outgoing_edges(vertex)]+[(e[2],1) for e in self.incoming_edges(vertex)]
+
+    def in_list(self,graph_list):
+        g = Graph(self)
+        flag=False
+        for c in graph_list:
+            if g.is_isomorphic(Graph(c)):
+                flag=True
+                break
+        return flag
+ 
+
+    def edge_split(self,label,vertex):
+        v = max(self.vertices())+1
+        l = max(self.edge_labels())+1
+        other = MyDiGraph(self,multiedges=True,loops=True)
+        edge = other.get_edge_with_label(label)
+        other.delete_edge(edge)
+        other.add_edges([(edge[0],v,l),(v,edge[1],l+1),(v,vertex,l+2)])
+        return other
+    
+    def extensions(self,graph_isomorphs=False):
+        """returns a list of all digraphs obtained from self by applying either a single vertex to 2-cycle move or a single edge split. If graph_isomorphs==False then it filters out isomorphs"""
+        out = []
+        for v in self.vertices():
+            for darts in subsets(self.darts_at(v)):
+                new = self.two_cycle_split(v,darts)
+                if new.in_list(out)==False:
+                    out.append(new)
+        for label in self.edge_labels():
+            for v in self.vertices():
+                new = self.edge_split(label,v)
+                if new.in_list(out)==False:
+                    out.append(new)
+        return out
+
+    @classmethod
+    def extend_list(cls,graph_list):
+        """returns a list of all digraphs obtained by applying a single 
+        extension move to one of the digraphs in the input list. Checks for 
+        isomorphs and eliminates them"""
+        out = []
+        for g in graph_list:
+            for h in g.extensions():
+                if h.in_list(out)==False:
+                    out.append(h)
+        return out
+
+
+
+
+
+class PebbleGame(object):
+    def __init__(self,vertex_set,k,l):
+        """Create a (k,l) sparsity checker for graphs on a given vertex set"""
+        self.k,self.l,self.vertex_set = k,l,vertex_set
+        self.digraph = DiGraph(multiedges=True,loops=True)
+        self.digraph.add_vertices(vertex_set)
+        self.vertex_weights = { label: k for label in self.digraph.vertices()}
+
+
+
+
+
+
+    def move_pebble_to(self,vertex_set):
+        search_list = list(self.digraph.depth_first_search(vertex_set))
+        for v in vertex_set:
+            search_list.remove(v)
+
+        search = iter(search_list)
+        w = search.next()
+        while self.vertex_weights[w] == 0:
+            w = search.next()
+        distances = [self.digraph.distance(x,w) for x in vertex_set]
+        #print(distances)
+        v = vertex_set[distances.index(min(distances))]
+        p = self.digraph.shortest_path(v,w)
+        #print(p)
+        for j in range(len(p)-1,0,-1):
+            self.digraph.delete_edge(p[j-1],p[j])
+            self.digraph.add_edge(p[j],p[j-1])
+        self.vertex_weights[v]+=1
+        self.vertex_weights[w]-=1
+
+
+        """moves a pebble into vertex_set according to the allowed moves of the (k,l) pebble game"""
+
+    def add_edge(self,u,v):
+        while self.vertex_weights[u]+self.vertex_weights[v] < max(self.l+1,1):
+            self.move_pebble_to([u,v])
+
+        if self.vertex_weights[u] >=self.vertex_weights[v]:
+            self.digraph.add_edge([u,v])
+            self.vertex_weights[u] -=1
+        else:
+            self.digraph.add_edge([v,u])
+            self.vertex_weights[v] -=1
+
+
+    def run(self,graph,test_only=True,reset=True):
+        if reset:
+            self.__init__(self.vertex_set,self.k,self.l)
+        for e in graph.edges():
+            try:
+                self.add_edge(e[0],e[1])
+            except:
+                if test_only:
+                    return False
+                else:
+                    print("cannot add edge (%s,%s)"%(e[0],e[1]))
+        return self.digraph
+
+
+
